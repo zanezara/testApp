c6c3af12eb253b1559c2d310e7ac2e66b46a9439
Zane Zara <zanezara@icloud.com>
2016-04-09 14:00:34 -0700

    Added name constant. Print the name to stdout

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let name = "Zane Zara"
+        print(name)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
